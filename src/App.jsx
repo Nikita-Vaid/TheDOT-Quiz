99dadd0397f639b0c9ee19e2419bd3f14fd07b0c
Nikita-Vaid <vaidnikita06@gmail.com>
2025-03-07 12:26:46 +0530

    Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Component/Pages/Login";
 import Layout from "./Component/Section/Layout";
-import RegForm from "./Component/Pages/RegForm";
+
 import TestSched from "./Component/Pages/TestSched";
 import Test from "./Component/Pages/Test";
 
@@ -13,7 +13,6 @@ function App() {
       <Routes>
       <Route path ="/" element ={<Layout/>}>
         <Route path="/" element={<Login/>}/>
-        <Route path="iacstexam" element={<RegForm/>} /> 
         <Route path="testschedule" element={<TestSched/>} /> 
         <Route path="test" element={<Test/>} />
         </Route>
